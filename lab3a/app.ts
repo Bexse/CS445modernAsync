e2592ec02cac702a076a9e1f0a8dc755424a55be
Bexse <89146106+Bexse@users.noreply.github.com>
2021-10-28 17:28:37 -0400

    Update app.ts

--- lab3a/app.ts
+++ lab3a/app.ts
@@ -3,7 +3,7 @@
  */
 
 let calcAverage = (a:number[]) =>{
-    let  sum:number = 0; ; 
+    let  sum:number = 0;  
     for (let i:number = 0; i < a.length; i++){
         sum+= a[i];
     }
@@ -13,7 +13,9 @@ let a:number[] = [17, 9, 4, 82, 15, 36];
 let b:number[] = [9, 4, 1, 7, 2, 3, 5, 8, 10, 6];
 
 let result = calcAverage(a);
-//let result = calcAverages(b);
+let result2 = calcAverages(b);
+console.log(result);
+console.log(result2);
 
 
 
