1389b175e1ef04498b4163025800acd8bd6618b7
Bexse <esubex977@gmail.com>
2021-10-28 19:46:46 -0400

    lab3a solutions

--- lab3a/app.js
+++ lab3a/app.js
@@ -1,13 +1,12 @@
 /*
- * app.js
+ * app.ts
  */
 var calcAverage = function (a) {
     var sum = 0;
-    ;
     for (var i = 0; i < a.length; i++) {
         sum += a[i];
     }
-    return (" The Average of [" + a + "] is " + (sum / a.length).toFixed(2));
+    return ("The Average of [" + a + "] is " + (sum / a.length).toFixed(2));
 };
 var a = [17, 9, 4, 82, 15, 36];
 var b = [9, 4, 1, 7, 2, 3, 5, 8, 10, 6];
@@ -15,3 +14,14 @@ var result = calcAverage(a);
 var result2 = calcAverage(b);
 console.log(result);
 console.log(result2);
+//enum Numeric Type
+//Given below is a set of declarations and 
+//initialization of constants for Tax rates used in a Payroll application.
+var FICA = 10, STATE = 5, MEDICARE = 12;
+var Taxrates;
+(function (Taxrates) {
+    Taxrates[Taxrates["FICA"] = 10] = "FICA";
+    Taxrates[Taxrates["STATE"] = 5] = "STATE";
+    Taxrates[Taxrates["MEDICARE"] = 12] = "MEDICARE";
+})(Taxrates || (Taxrates = {}));
+;
--- lab3a/app.ts
+++ lab3a/app.ts
@@ -7,7 +7,7 @@ let calcAverage = (a:number[]) =>{
     for (let i:number = 0; i < a.length; i++){
         sum+= a[i];
     }
-    return (` The Average of [${a}] is ${(sum/a.length).toFixed(2)}`);
+    return (`The Average of [${a}] is ${(sum/a.length).toFixed(2)}`);
 }
 let a:number[] = [17, 9, 4, 82, 15, 36]; 
 let b:number[] = [9, 4, 1, 7, 2, 3, 5, 8, 10, 6];
@@ -17,7 +17,10 @@ let result2 = calcAverage(b);
 console.log(result);
 console.log(result2);
 
-
-
+//enum Numeric Type
+//Given below is a set of declarations and 
+//initialization of constants for Tax rates used in a Payroll application.
+const FICA = 10, STATE = 5, MEDICARE = 12;
+enum Taxrates {FICA = 10, STATE = 5, MEDICARE = 12};
 
 
